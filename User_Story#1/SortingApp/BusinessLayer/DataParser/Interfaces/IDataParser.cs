8214750fcb2e9265725d2a506908cd3cb89852b8
Anton Petrenko <anton.petrenko@sigma.software>
2019-03-26 11:25:40 +0200

    added dataParser

--- User_Story#1/SortingApp/BusinessLayer/DataParser/Interfaces/IDataParser.cs
+++ User_Story#1/SortingApp/BusinessLayer/DataParser/Interfaces/IDataParser.cs
@@ -1,18 +1,14 @@
 ﻿namespace BusinessLayer.DataParser.Interfaces
 {
     /// <summary>
-    /// Represents general interface for validation classes.
+    /// Represents general interface for data parser classes.
     /// </summary>
     /// <owner>Anton Petrenko</owner>
     public interface IDataParser
     {
         /// <summary>
-        /// Determines whether data exists.
+        /// Convert data to collection of type Double.
         /// </summary>
-        /// <param name="content">The content.</param>
-        /// <returns>
-        ///   <c>true</c> if data exists; otherwise, <c>false</c>.
-        /// </returns>
         /// <owner>Anton Petrenko</owner>
         void ConvertDataToCollectionOfDouble();
     }
---
+++ User_Story#1/SortingApp/BusinessLayer/DataParser/ParserToIEnumerable.cs
@@ -0,0 +1,45 @@
+﻿using BusinessLayer.DataParser.Interfaces;
+using System;
+using System.Collections.Generic;
+
+namespace BusinessLayer.DataParser
+{
+    /// <summary>
+    /// Parse collection to array of Double type
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <owner>Anton Petrenko</owner>
+    /// <seealso cref="BusinessLayer.DataParser.Interfaces.IDataParser" />
+    public class ParserToIEnumerable<T> : IDataParser
+    {
+        /// <summary>
+        /// The list of items from file
+        /// </summary>
+        /// <owner>Anton Petrenko</owner>
+        private T[] listOfItemsFromFile;
+
+        /// <summary>
+        /// The separators array
+        /// </summary>
+        /// <owner>Anton Petrenko</owner>
+        private readonly char[] separators;
+
+        /// <summary>
+        /// Convert data to collection of type Double.
+        /// </summary>
+        /// <owner>Anton Petrenko</owner>
+        public void ConvertDataToCollectionOfDouble()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the ParserToIEnumerable class with array of separators.
+        /// </summary>
+        /// <owner>Anton Petrenko</owner>
+        public ParserToIEnumerable()
+        {
+            separators = new char[] { '.', ',', ';', '/', ' ' };
+        }
+    }
+}
