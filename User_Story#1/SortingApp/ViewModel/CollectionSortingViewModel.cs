5fd13991c1e200c5c07a1397e7a4783b3a2b5d6d
Anton Petrenko <anton.petrenko@sigma.software>
2019-03-28 13:38:15 +0200

    updates in ViewModel part

--- User_Story#1/SortingApp/ViewModel/CollectionSortingViewModel.cs
+++ User_Story#1/SortingApp/ViewModel/CollectionSortingViewModel.cs
@@ -1,4 +1,11 @@
 ﻿using BusinessLayer;
+using BusinessLayer.DataParser;
+using BusinessLayer.Reader;
+using BusinessLayer.SortingAlgorithms;
+using BusinessLayer.Utilities.Parser.Interfaces;
+using BusinessLayer.Utilities.Validator.Interfaces;
+using BusinessLayer.Validator;
+using BusinessLayer.Writer;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 
@@ -20,32 +27,65 @@ namespace ViewModel
         public ObservableCollection<T> CollectionOfNumbers { get; set; }
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="CollectionSortingViewModel{T}"/> class.
+        /// The handler.
         /// </summary>
         /// <owner>Anton Petrenko</owner>
-        public CollectionSortingViewModel(CollectionSortHandler<T> handler)
-        {
-            this.CollectionOfNumbers = new ObservableCollection<T>();
-            this.handler = handler;
-        }
+        private readonly CollectionSortHandler<T> handler;
 
         /// <summary>
-        /// The handler
+        /// The parser.
         /// </summary>
         /// <owner>Anton Petrenko</owner>
-        private CollectionSortHandler<T> handler;
+        private readonly IDataParser<T> parser;
 
         /// <summary>
         /// Occurs when a property value changes.
         /// </summary>
         /// <owner>Anton Petrenko</owner>
         public event PropertyChangedEventHandler PropertyChanged;
-        
+
         /// <summary>
-        /// The type of sort from enum
+        /// The reader.
+        /// </summary>
+        /// <owner>Anton Petrenko</owner>
+        private readonly DataReader reader;
+
+        /// <summary>
+        /// The sorter.
+        /// </summary>
+        /// <owner>Anton Petrenko</owner>
+        private readonly CollectionSorter<T> sorter;
+
+        /// <summary>
+        /// The type of sort from enum.
         /// </summary>
         /// <owner>Anton Petrenko</owner>
         private SortType type;
+
+        /// <summary>
+        /// The validator.
+        /// </summary>
+        /// <owner>Anton Petrenko</owner>
+        private readonly IValidator validator;
+
+        /// <summary>
+        /// The writer.
+        /// </summary>
+        /// <owner>Anton Petrenko</owner>
+        private readonly DataWriter<T> writer;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CollectionSortingViewModel{T}"/> class.
+        /// </summary>
+        /// <owner>Anton Petrenko</owner>
+        public CollectionSortingViewModel()
+        {
+            this.validator = new LocalFileValidator();
+            this.reader = new DataReader(validator);
+            this.writer = new DataWriter<T>();
+            this.parser = new ArrayParser<T>();
+            this.handler = new CollectionSortHandler<T>(reader, writer, sorter, parser);
+        }
 
         /// <summary>
         /// Gets the read command.
