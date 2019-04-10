268db34e50b9acf1e11e337ef7c94a24d229214a
Anton Petrenko <anton.petrenko@sigma.software>
2019-04-10 19:51:58 +0300

    added Test project

---
+++ User_Story#1/SortingApp/SortinfApp.UnitTests/DataReader_Tests/ReadContent_ContentIsExist_StringNotEmpty.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SortinfApp.UnitTests.DataReader_Tests
+{
+    [TestClass]
+    public class ReadContent_ContentIsExist_StringNotEmpty
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+    }
+}
