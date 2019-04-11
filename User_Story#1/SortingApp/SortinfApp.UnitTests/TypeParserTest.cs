ca4e01e99fb78b261b3b7ca2a185cc2c1fa96f60
Anton Petrenko <anton.petrenko@sigma.software>
2019-04-11 12:10:20 +0300

    added Tests for TypeParser

---
+++ User_Story#1/SortingApp/SortinfApp.UnitTests/TypeParserTest.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using BusinessLayer.Extensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SortinfApp.UnitTests
+{
+    /// <summary>
+    /// Tests for TypeParser class.
+    /// </summary>
+    /// <owner>Anton Petrenko</owner>
+    [TestClass]
+    public class TypeParserTest
+    {
+        /// <summary>
+        /// Tests ChangeType method if pass null value it will throw ArgumentNullException.
+        /// </summary>
+        /// <owner>Anton Petrenko</owner>
+        [TestMethod]
+        public void ChangeType_PassNullValue_ThrowArgumentNullException()
+        {
+            //Arrange
+            object integer = null;
+
+            //Act
+
+            //Assert
+            Assert.ThrowsException<ArgumentNullException>( () => TypeParser.ChangeType<int>(integer));
+        }
+
+        /// <summary>
+        /// Tests ChangeType method if pass string with number it will make integer value.
+        /// </summary>
+        /// <owner>Anton Petrenko</owner>
+        [TestMethod]
+        public void ChangeType_PassStringWithInt_MakeInt()
+        {
+            //Arrange
+            var inputValue = "150";
+            var number = 150;
+
+            //Act
+            var outputNumber = TypeParser.ChangeType<int>(inputValue);
+
+            //Assert
+            Assert.AreEqual(number, outputNumber);
+        }
+
+        /// <summary>
+        /// Tests ChangeType method if pass string with int it will make value type of int.
+        /// </summary>
+        /// <owner>Anton Petrenko</owner>
+        [TestMethod]
+        public void ChangeType_PassStringWithInt_MakeValueTypeOfInt()
+        {
+            //Arrange
+            var inputValue = "10";
+            var type = typeof(int);
+
+            //Act
+            var outputNumber = TypeParser.ChangeType<int>(inputValue);
+
+            //Assert
+            Assert.AreEqual(type, outputNumber.GetType());
+        }
+    }
+}
